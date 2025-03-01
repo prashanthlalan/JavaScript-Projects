d2f8f3f3eb319fea33a4eb092cc2c3fb19dfb08a
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-02-28 23:38:41 -0500

    Multiple Variables Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -5,5 +5,7 @@ var B = "This is a String 2";
 window.alert(B);
 document.write("This is test \"String\" for the assignment");
 document.write("Combining this string with " + "next string");
-var B = "Concatenated" + "String"
-document.write(B)
+var B = "Concatenated" + " String";
+document.write(B);
+var name ="Alex John", address="631, unknown street", phoneNumber="6479999999";
+document.write(phoneNumber);
