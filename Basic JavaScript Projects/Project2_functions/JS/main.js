f14bf82f2e95c1b9e8ad3274280239cd07f7c37b
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-01 02:55:25 -0500

    Function Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function  My_Assignment_Function() {
+    var str1 = "Button Text";
+    var str2 = "Button Text1"
+    document.getElementById("Paragraph1").innerHTML = str1;
+    document.getElementById("Paragraph2").innerHTML = str2;
+}
