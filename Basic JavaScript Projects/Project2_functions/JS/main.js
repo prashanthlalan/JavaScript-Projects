d095adcd12833936c18448a5ffa69b60560cae35
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-01 12:38:48 -0500

    Project2_Functions Assignment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,3 +1,6 @@
+//main.js   
+
+//Write a function that uses the += operator
 function  My_Assignment_Function() {
     var str1 = "Button Text";
     var str2 = "Button Text1"
@@ -5,7 +8,7 @@ function  My_Assignment_Function() {
     document.getElementById("Paragraph2").innerHTML = str2;
 }
 function myFunction() {
-    var sentence ="I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+    var sentence ="I am learning";//create the variable sentence
+    sentence += " a lot from this book!";//using the += operator to add the variable
+    document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementByID()
 }
