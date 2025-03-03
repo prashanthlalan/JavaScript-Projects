d5b86436afe87faa820efa76974a6596c8f68249
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-02 21:06:44 -0500

    Multiplication and Division Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -6,3 +6,11 @@ function myFunction1 () {
     var substraction = 8-3;
     document.getElementById("Math2").innerHTML = "8 - 3 =" + substraction;
 }
+function myMultiplication () {
+    var multiplication = 8*3;
+    document.getElementById("Math3").innerHTML = "8 * 3 =" + multiplication;
+}
+function myDivision () {
+    var division = 16/2;
+    document.getElementById("Math4").innerHTML = "16 / 2 =" + division;
+}
