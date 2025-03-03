6d75ec4f3bae7ff7ef01d815cf4b984baac6e3d5
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-02 20:46:40 -0500

    Subtraction Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,7 @@ function myFunction () {
     var addition = 3+3;
     document.getElementById("Math").innerHTML = "3 + 3 =" + addition;
 }
+function myFunction1 () {
+    var substraction = 8-3;
+    document.getElementById("Math2").innerHTML = "8 - 3 =" + substraction;
+}
