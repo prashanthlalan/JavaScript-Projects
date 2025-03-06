da94c61789acac88f1195fc92178b9f737c3c453
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-06 14:44:56 -0500

    Infinity Assignment

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -2,3 +2,11 @@ function Coersion() {
     var string_add = document.write(typeof("20" + 5));
     document.getElementById("String_Number").innerHTML = string_add;
 }
+function Positive_infinity() {
+    var infinity_positive = document.write(3E310);
+    document.getElementById("Infinity").innerHTML = infinity_positive;
+}
+function Negative_infinity() {
+    var infinity_negative = document.write(-3E310);
+    document.getElementById("Minus_Infinity").innerHTML = infinity_negative;
+}
