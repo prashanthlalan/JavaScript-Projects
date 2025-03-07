136a87c274191f5e69bf82269e371183211c3c81
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-06 20:10:36 -0500

    Triple Equal Signs Assignment

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -19,3 +19,19 @@ function Double_Equal_Signs(){
     var isEqual =document.write(10==5*2)
     document.getElementById("Equals").innerHTML = isEqual;
 }
+function Same_data_type_value() {
+    var sdtsv = document.write(10===10)
+    document.getElementById("same").innerHTML =sdtsv;
+}
+function different_data_type_value() {
+    var ddtdv = document.write(10==="12")
+    document.getElementById("differentBoth").innerHTML =ddtdv;
+}
+function different_data_type_same_value() {
+    var ddtsv = document.write(10==="10")
+    document.getElementById("differentBoth").innerHTML =ddtsv;
+}
+function same_data_type_different_value() {
+    var sdtdv = document.write("10"==="12")
+    document.getElementById("differentBoth").innerHTML =sdtdv;
+}
