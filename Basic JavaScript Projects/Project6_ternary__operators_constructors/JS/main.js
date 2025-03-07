d402bea7223064aaac2d648d65702dff952656da
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-07 13:43:12 -0500

    New Keyword Assignment

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -4,3 +4,17 @@ function Ride_Function(){
     Can_ride = (Height < 52) ? "You are to short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+function Vehicle(Make, Model, Year, Color){
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trial Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction(){
+    document.getElementById("New_and_This").innerHTML =
+    "Emily drives a " + Emily.Vehicle_Color + "-colored " + Emily.Vehicle_Model +
+    " manufractured in " + Emily.Vehicle_Year;
+}
