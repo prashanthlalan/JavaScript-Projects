27b20478c5dd6d37307a7515479f86ce7e48430d
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-07 01:37:40 -0500

    Project5_Comparisons_Type_Coercion Assignment

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,24 +1,40 @@
+//Variable with boolean data type
+var x = true;
+
+//printing the data type of string_add and and expression combining a string and a number
 function Coersion() {
     var string_add = document.write(typeof("20" + 5));
     document.getElementById("String_Number").innerHTML = string_add;
 }
+
+//utilizing the positive infinity
 function Positive_infinity() {
     var infinity_positive = document.write(3E310);
     document.getElementById("Infinity").innerHTML = infinity_positive;
 }
+
+//utilizing the negative infinity
 function Negative_infinity() {
     var infinity_negative = document.write(-3E310);
     document.getElementById("Minus_Infinity").innerHTML = infinity_negative;
 }
+
+//utilizing the Boolean Logic
 function Boolean_Logic() {
     var Boolean = document.write(10>12);
     document.getElementById("Boolean_Operator").innerHTML = Boolean;
 }
+
+//utilizing the Console.log
 console.log(2+2)
+
+//utilizing the ==
 function Double_Equal_Signs(){
     var isEqual =document.write(10==5*2)
     document.getElementById("Equals").innerHTML = isEqual;
 }
+
+//utilizing the ===
 function Same_data_type_value() {
     var sdtsv = document.write(10===10)
     document.getElementById("same").innerHTML =sdtsv;
@@ -35,11 +51,20 @@ function same_data_type_different_value() {
     var sdtdv = document.write("10"==="12")
     document.getElementById("differentBoth").innerHTML =sdtdv;
 }
+
+//utilizing the && operator
 function andLogic() {
     var andl = document.write(15>10 && 6<12)
     document.getElementById("and").innerHTML =andl;
 }
+
+//utilizing || operator
 function orLogic() {
     var orl = document.write(6>10 || 6>12)
     document.getElementById("or").innerHTML =orl;
+}
+
+//utilizing the ! operator
+function notFunction() {
+    document.getElementById("not").inner HTML = !(50>20);
 }
