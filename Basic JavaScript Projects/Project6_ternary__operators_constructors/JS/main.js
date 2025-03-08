f4bc98480a16d67aa3125499e6433e93aadd94cf
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-07 23:51:01 -0500

    Nested Functions Assignment

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -18,3 +18,12 @@ function myFunction(){
     "Emily drives a " + Emily.Vehicle_Color + "-colored " + Emily.Vehicle_Model +
     " manufractured in " + Emily.Vehicle_Year;
 }
+function count_Function() {
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point +=1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
