dfdb8b59527653064fb71c16eedc60dbc16ed6e3
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-08 00:03:11 -0500

    Final Project6_Ternary_Operators_Constructors Assignment

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,9 +1,12 @@
+//A function with HTML and JS using a ternary operation with input from the browser
 function Ride_Function(){
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
     Can_ride = (Height < 52) ? "You are to short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+//A constructor function utilizing "new" and "this" keywords
 function Vehicle(Make, Model, Year, Color){
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
@@ -13,11 +16,15 @@ function Vehicle(Make, Model, Year, Color){
 var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trial Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+
+//A function to display the results of the constructor in an HTML element
 function myFunction(){
     document.getElementById("New_and_This").innerHTML =
-    "Emily drives a " + Emily.Vehicle_Color + "-colored " + Emily.Vehicle_Model +
-    " manufractured in " + Emily.Vehicle_Year;
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufractured in " + Erik.Vehicle_Year;
 }
+
+//A nested function
 function count_Function() {
     document.getElementById("Nested_Function").innerHTML = Count();
     function Count() {
