8aea7e698778ef7765411d1219d02ad108fa2965
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-09 16:41:18 -0400

    For Loop Assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -7,3 +7,12 @@ function Call_Loop() {
     } 
     document.getElementById("Loop").innerHTML = Digit;
 }
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;        
+}
