6883c5321efd8567cc76bd90a04a8a0ce21d777f
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-09 00:11:51 -0500

    Project8_String_Methods Assignment

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,3 +1,4 @@
+//Utilizing the .concat() method
 function full_sentence(){
     var part_1 = "This sentence is made from ";
     var part_2 = "one broken ";
@@ -5,15 +6,21 @@ function full_sentence(){
     var whole_sentence = part_1.concat(part_2, part_3);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
+
+//Utilizing the .slice() method
 function slice_Method(){
     var Sentence = "All work and no play makes Johnny a dull boy.";
     var Section = Sentence.slice(27,33);
     document.getElementById("Slice").innerHTML = Section;
 }
+
+//Utilizing the .toString() method
 function string_Method() {
     var X =182;
     document.getElementById("Numbers_to_string").innerHTML = X.toString();
 }
+
+//Utlizing the toPrecision() method
 function precision_Method(){
     var X = 12938.3012987376112;
     document.getElementById("Precision").innerHTML = X.toPrecision(10);
