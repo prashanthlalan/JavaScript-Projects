3c97811ec747eeb556abcab8a5697e292d5b828d
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-09 18:24:15 -0400

    Object Assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -39,3 +39,15 @@ document.write(X);
     document.write("<br>" + X);
 }
 document.write("<br>" + X)
+function let_function() {
+    let car = {
+        make: "Dodge ",
+        model: "Viper",
+        year: "2021",
+        color: "Red",
+        description: function() {
+            return "The car is a " + this.year + " "+ this.color + " " + this.make + this.model;
+        }
+    };
+document.getElementById("Car_Object").innerHTML = car.description();
+}
