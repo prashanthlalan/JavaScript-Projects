90717d3c1a25756fb340455a1e1c545588bbcc2d
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-08 23:29:16 -0500

    slice() Method Assignment

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -5,3 +5,8 @@ function full_sentence(){
     var whole_sentence = part_1.concat(part_2, part_3);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
+function slice_Method(){
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
