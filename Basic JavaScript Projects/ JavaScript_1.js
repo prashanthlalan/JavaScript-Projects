640778c5468cbf5e04c2ef452f701785d0b2ff9a
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-10 00:40:03 -0400

    Gradient Color Challenge

--- Basic JavaScript Projects/ JavaScript_1.js
+++ Basic JavaScript Projects/ JavaScript_1.js
@@ -9,3 +9,17 @@ function Canvas_Graphics(){
     ctx.arc(250, 125, 100, 0, 5 * Math.PI);
     ctx.stroke();
 }
+function Canvas_Gradient() {
+    const canvas = document.getElementById("Gradient");
+    const ctx = canvas.getContext("2d");
+    
+    // Create Gradient
+    const my_gradient = ctx.createLinearGradient(0, 0, 500, 0);
+    my_gradient.addColorStop(0, "green");
+    my_gradient.addColorStop(0.5, "yellow");
+    my_gradient.addColorStop(1, "purple");
+    
+    // Fill Rectangle
+    ctx.fillStyle = my_gradient;
+    ctx.fillRect(0, 0, 500, 250); 
+}
