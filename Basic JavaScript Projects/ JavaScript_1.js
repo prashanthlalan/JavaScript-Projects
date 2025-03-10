8c4b30b2edc1e618038448ec5f08742336127400
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-03-10 00:16:14 -0400

    Canvas Challenge

--- Basic JavaScript Projects/ JavaScript_1.js
+++ Basic JavaScript Projects/ JavaScript_1.js
@@ -2,3 +2,10 @@ function Hello_World_Function() {
     var A = document.getElementsByClassName("click");
     A[0].innerHTML = "The text has changed!";
 }
+function Canvas_Graphics(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(250, 125, 100, 0, 5 * Math.PI);
+    ctx.stroke();
+}
